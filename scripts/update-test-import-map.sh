ffb4999c73a69af35ce0f3f91f1216421e4bb9e4
Nicky Kimaina <nkimaina@ampath.or.ke>
2020-01-30 19:04:59 +0300

    Adding support for deployment to prod

---
+++ scripts/update-test-import-map.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    cd ./test/openmrs/frontend/
+    STATUS_CODE=$(curl --output /dev/null --write-out %{http_code} -u $DEPLOYER_USERNAME:$DEPLOYER_PASSWORD -d '@import-map.json' -X PATCH $DEPLOYER_HOST/import-map.json\?env=test -H "Accept:application/json" -H "Content-Type:application/json")
+    echo $STATUS_CODE
+else
+    echo 200
+fi
