5d2f1d4f964f2f57b7cf123636c372451abcd88c
Nicky Kimaina <nkimaina@ampath.or.ke>
2020-01-30 20:23:19 +0300

    w.i.p

---
+++ scripts/update-beta-import-map.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ "$DEPLOY_BETA" == "1" ]
+then
+    cd ./prod/openmrs/frontend/
+    STATUS_CODE=$(curl --output /dev/null --write-out %{http_code} -u $DEPLOYER_USERNAME:$DEPLOYER_PASSWORD -d '@import-map.json' -X PATCH $DEPLOYER_HOST/import-map.json\?env=beta -H "Accept:application/json" -H "Content-Type:application/json")
+    echo $STATUS_CODE
+else
+    echo 200
+fi
--- scripts/update-test-import-map.sh
+++ scripts/update-test-import-map.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]
+if [ "$TRAVIS_BRANCH" == "deploy-prod" ]
 then
     cd ./test/openmrs/frontend/
     STATUS_CODE=$(curl --output /dev/null --write-out %{http_code} -u $DEPLOYER_USERNAME:$DEPLOYER_PASSWORD -d '@import-map.json' -X PATCH $DEPLOYER_HOST/import-map.json\?env=test -H "Accept:application/json" -H "Content-Type:application/json")
